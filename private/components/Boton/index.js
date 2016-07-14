2aef441a9875c448cab28eb8c8de14b347fdb068
Jose Droguett <jdroguet@produccion.datactil.com>
2016-07-14 11:01:58 -0400

    Ordenar componente en carpeta. Rename componente. Add estilo del boton por defecto. Update tipos de boton

---
+++ private/components/Boton/index.js
@@ -0,0 +1,88 @@
+import React, { Component, PropTypes } from 'react';
+import styles from './styles.css';
+
+/**
+ * Boton Component
+ */
+class Boton extends Component {
+  /**
+   * Define what HTML will be  rendered to DOM
+   * @method render
+   * @return {[type]} [description]
+   */
+  render() {
+    const className = this.props.class ? this.props.class : 'button';
+    const {
+      style = function (){},
+      onHover = function (){},
+      onClick = function (){},
+      text,
+      href,
+      src,
+      icon,
+    } = this.props;
+    
+    let contentBoton;
+    if(href){
+      if(text || icon){
+        contentBoton = (
+          <a className={className} onMouseOver={onHover} href={href} onClick={onClick} style={style()}>
+            <i key='icono' className={icon}></i>
+            {text || ''}
+          </a>
+        )
+      }else if(src){
+        contentBoton = (
+          <a className={className} onMouseOver={onHover} href={href} onClick={onClick} style={style()}>
+            <img src={src} />
+          </a>
+        )
+      }else{
+        contentBoton = (
+          <a className={className} onMouseOver={onHover} href={href} onClick={onClick} style={style()}>
+            Boton
+          </a>
+        )
+      }
+    }else{
+      if(text || icon){
+        contentBoton = (
+          <button className={className} onMouseOver={onHover} onClick={onClick} style={style()}>
+            <i key='icono' className={icon}></i>
+            {text || ''}
+          </button>
+        )
+      }else if(src){
+        contentBoton = (
+          <button className={className} onMouseOver={onHover} onClick={onClick} style={style()}>
+            <img src={src} />
+          </button>
+        )
+      }else{
+        contentBoton = (
+          <button className={className} onMouseOver={onHover} onClick={onClick} style={style()}>
+            Boton
+          </button>
+        )
+      }
+    }
+    return (
+      <div className='buttonWrapper'>
+        {contentBoton}
+      </div>
+    );
+  }
+}
+
+Boton.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.func,
+  onHover: PropTypes.func,
+  onClick: PropTypes.func,
+  text: PropTypes.string,
+  href: PropTypes.string,
+  src: PropTypes.string,
+  icon: PropTypes.string,
+};
+
+export default Boton;
